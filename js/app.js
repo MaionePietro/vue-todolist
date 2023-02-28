ad2b4c32a827bdda542233ae465ac3be0708aac8
MaionePietro <pietromaionespam@gmail.com>
2023-02-28 01:46:05 +0100

    FEAT metods addTask COMPLETE

--- js/app.js
+++ js/app.js
@@ -1,41 +1,45 @@
 const { createApp } = Vue
-    
-      createApp({
-        data() {
-          return {
-            mytask: [
-				{
-					pText: 'prova prova',
-					verifica: false,
-				},
-                {
-					pText: 'prova prova list',
-					verifica: false,
-				},
-                {
-					pText: 'prova prova super list',
-					verifica: false,
-				}
-			],
-            inputValue: '',
-          }
-        },
-        methods: {
-            addTask() {
-                const newTask = this.inputValue
-            },
-            removeTask(indice) {
-                console.log(this.mytask[indice])
-                this.mytask.splice(indice)
-            },
-            decoration(index){
-                console.log(this.mytask[index].verifica)
-                if(this.mytask[index].verifica === true ){
-                  this.mytask[index].verifica = false
-                }else{
-                  this.mytask[index].verifica = true
-                }
-            }
 
+createApp({
+  data() {
+    return {
+      mytask: [
+        {
+          pText: 'prova prova',
+          verifica: false,
         },
-      }).mount('#app')
+        {
+          pText: 'prova prova list',
+          verifica: false,
+        },
+        {
+          pText: 'prova prova super list',
+          verifica: false,
+        }
+      ],
+      inputValue: '',
+    }
+  },
+  methods: {
+    addTask() {
+      const newTask = {
+        pText: this.inputValue,
+        verifica: false,
+      };
+      this.mytask.push(newTask);
+    },
+    removeTask(indice) {
+      console.log(this.mytask[indice])
+      this.mytask.splice(indice)
+    },
+    decoration(index) {
+      console.log(this.mytask[index].verifica)
+      if (this.mytask[index].verifica === true) {
+        this.mytask[index].verifica = false
+      } else {
+        this.mytask[index].verifica = true
+      }
+    }
+
+  },
+}).mount('#app')
